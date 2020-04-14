f5616772a1b1cd51e28a4a54e9e30d41ad8f5b82
fernandesnetobr <eliasfernandescoutinho@gmail.com>
2020-04-14 20:05:30 -0300

    stateFull

--- lib/main.dart
+++ lib/main.dart
@@ -4,48 +4,48 @@ main() {
   runApp(PerguntaApp());
 }
 
-@override
-class PerguntaApp extends StatefulWidget {
-
+class PerguntaAppState extends State<PerguntaApp> {
   var perguntaSelecionada = 0;
 
-  void responder(){
+  void responder() {
     perguntaSelecionada++;
-
-
   }
-
+  @override
   Widget build(BuildContext context) {
     final perguntas = [
       "Qual é a sua cor favorita?",
       "Qual é o seu animal favorito?",
-
     ];
-
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Perguntas"),
-        ),
-        body: Column(
-          children: <Widget>[
-           Text(perguntas.elementAt(0)),
-            RaisedButton(
-              child: Text("Resposta 1"),
-              onPressed: responder,
-            ),
-            RaisedButton(
-              child: Text("Resposta 2"),
-              onPressed: responder,
-            ),
-            RaisedButton(
-              child: Text("Resposta 3"),
-              onPressed: responder,
-            ),
-          ],
-        )
-      ),
+          appBar: AppBar(
+            title: Text("Perguntas"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text(perguntas.elementAt(0)),
+              RaisedButton(
+                child: Text("Resposta 1"),
+                onPressed: responder,
+              ),
+              RaisedButton(
+                child: Text("Resposta 2"),
+                onPressed: responder,
+              ),
+              RaisedButton(
+                child: Text("Resposta 3"),
+                onPressed: responder,
+              ),
+            ],
+          )),
     );
+  }
+}
+
+
+class PerguntaApp extends StatefulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
